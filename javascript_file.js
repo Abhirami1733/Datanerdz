f8c0ccbbb1260a65bfa2dd8938d865713bc080cc
ABHIRAMI E <136597873+Abhirami1733@users.noreply.github.com>
2024-02-14 11:32:16 +0530

    Add files via upload

---
+++ javascript_file.js
@@ -0,0 +1,42 @@
+let slideIndex = 1;
+let timer;
+
+showSlides(slideIndex);
+
+function nextSlide() {
+  showSlides(slideIndex += 1);
+}
+
+function prevSlide() {
+  showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.querySelectorAll('.carousel-slide img');
+  const dots = document.querySelectorAll('.dot');
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex-1].style.display = 'block';
+  dots[slideIndex-1].className += ' active';
+}
+
+function startCarousel() {
+  timer = setInterval(nextSlide, 3000);
+}
+
+function pauseCarousel() {
+  clearInterval(timer);
+}
+
+startCarousel();
